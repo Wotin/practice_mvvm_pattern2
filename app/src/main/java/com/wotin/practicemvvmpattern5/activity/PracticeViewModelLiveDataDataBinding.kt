ecd7b7361729352f77219274980e29b83a1c07c1
simplepro <simplepro1254@gmail.com>
2020-11-03 13:54:00 +0900

    ViewModel LiveData DataBinding 추가

--- app/src/main/java/com/wotin/practicemvvmpattern5/activity/PracticeViewModelLiveDataDataBinding.kt
+++ app/src/main/java/com/wotin/practicemvvmpattern5/activity/PracticeViewModelLiveDataDataBinding.kt
@@ -2,8 +2,11 @@ package com.wotin.practicemvvmpattern5.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.Observable
+import androidx.lifecycle.Observer
 import com.wotin.practicemvvmpattern5.R
 import com.wotin.practicemvvmpattern5.databinding.ActivityPracticeViewModelLiveDataDataBindingBinding
 import com.wotin.practicemvvmpattern5.viewModel.SubViewModel
@@ -18,6 +21,10 @@ class PracticeViewModelLiveDataDataBinding : AppCompatActivity() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_practice_view_model_live_data_data_binding)
         mBinding.lifecycleOwner = this
         mBinding.viewModel = model
+
+        model.count.observe(this, Observer {count ->
+            Log.d("TAG","count is $count")
+        })
 
     }
 
